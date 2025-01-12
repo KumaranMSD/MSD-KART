1377346bc7dddc21ed473b72de186d906e9e3ee8
Udayar Kumaran <judayarkumaran@gmail.com>
2025-01-12 13:19:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,6 @@ if (process.env.NODE_ENV == 'production') {
     });
 }
 
-app.listen(https://msd-kart.onrender.com, () => {
-    console.log(`Server listening to Port MSD-Kart in ${process.env.NODE_ENV}`)
+app.listen(process.'https://msd-kart.onrender.com/', () => {
+    console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 });
