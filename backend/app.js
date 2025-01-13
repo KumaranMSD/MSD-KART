36f87c3066019cd6483000eda5a9e18a23f5145c
Udayar Kumaran <judayarkumaran@gmail.com>
2025-01-13 12:49:48 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,6 @@ if (process.env.NODE_ENV == 'production') {
     });
 }
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server listening to Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
 });
